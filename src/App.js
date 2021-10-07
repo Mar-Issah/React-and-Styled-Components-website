62ac95467e723b14bda49a79798ff1d47ad5b72a
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-07 18:50:27 -0400

    global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styles/Header";
 
 const theme = {
 	colors: {
@@ -11,6 +12,7 @@ const theme = {
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
+			<GlobalStyles />
 			<Header />
 		</ThemeProvider>
 	);
