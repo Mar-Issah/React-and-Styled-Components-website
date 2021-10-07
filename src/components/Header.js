b6fa35664e6ad6c2233e1471e9e3ea3d9ddc49af
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-07 15:45:14 -0400

    nesting

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./styles/StyledHeader";
+
+const Header = () => {
+	return (
+		<StyledHeader>
+			<h1>Huddle</h1>
+		</StyledHeader>
+	);
+};
+
+export default Header;
---
+++ src/components/styles/Header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+	background-color: #ebfbff;
+	padding: 50px 0;
+
+	h1 {
+		color: red;
+	}
+
+	&:hover {
+		background: black;
+	}
+`;
