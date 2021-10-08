cb3b3d17a064d176e0b8eaefe4f6f450d21c3e7c
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-07 23:13:42 -0400

    footer

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,9 @@ import { StyledCard } from "../components/styles/Card.Styles";
 
 export default function Card({ item: { id, title, body, image } }) {
 	return (
+		// if the modolus of the index is testing 0 which is an even then reverse the flex so if the condition is true the row-reverse will take effect
+
+		//&& means then, used in tenery operator where there is no else
 		<StyledCard layout={id % 2 === 0 && "row-reverse"}>
 			<div>
 				<h2>{title}</h2>
