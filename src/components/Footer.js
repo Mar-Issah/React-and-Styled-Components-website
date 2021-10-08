54d85edfb7e9cfc493e21f43e660efe964601db4
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-10-07 23:23:07 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,13 @@ import SocialIcons from "./SocialIcons";
 import { Container } from "./styles/Container.styled";
 import { Flex } from "./styles/Flex.styled";
 import { StyledFooter } from "./styles/Footer.styled";
+import { Logo } from "./styles/Header.styled";
 
 export default function Footer() {
 	return (
 		<StyledFooter>
 			<Container>
-				<img src="./images/logo_white.svg" alt="" />
+				<Logo>Siya</Logo>
 
 				<Flex>
 					<ul>
